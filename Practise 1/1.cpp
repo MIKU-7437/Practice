f67fbcd0755c7f3841e0b437835bc63f8f7cd436
Miku <nurshanov.dias@gmail.com>
2023-06-06 23:32:53 +0600

    pr2

--- Practise 1/1.cpp
+++ Practise 1/1.cpp
@@ -4,8 +4,8 @@ int Fib(int i){
     int value = 0;
     if (i < 1) return 0;
     if (i == 1) return 1;
-    return Fib(i-1) + Fib(i+2)
+    return Fib(i-1) + Fib(i+2);
 }
-int main{
-    cout << Fib(2) << endl;
+int main(){
+    cout << Fib(1) << endl;
 }
